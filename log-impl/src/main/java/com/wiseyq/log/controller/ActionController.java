480c60d0f239d5428f1bcb2e66420aca7ea582f0
no502zhang <no502zhang@163.com>
2018-07-10 22:45:53 +0800

    行为定义调试更新

--- log-impl/src/main/java/com/wiseyq/log/controller/ActionController.java
+++ log-impl/src/main/java/com/wiseyq/log/controller/ActionController.java
@@ -3,11 +3,7 @@ package com.wiseyq.log.controller;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.github.pagehelper.PageInfo;
 import com.wiseyq.log.model.ActionDefine;
@@ -20,7 +16,7 @@ public class ActionController {
     private ActionService actionService;
 
     @PostMapping("/add/")
-    public ResponseEntity<ActionDefine> add(ActionDefine action) {
+    public ResponseEntity<ActionDefine> add(@RequestBody ActionDefine action) {
         actionService.insert(action);
         return ResponseEntity.ok().body(action);
     }
@@ -32,7 +28,7 @@ public class ActionController {
     }
 
     @PostMapping("/update/{id}")
-    public ResponseEntity<ActionDefine> update(@PathVariable("id") int id, ActionDefine action) {
+    public ResponseEntity<ActionDefine> update(@PathVariable("id") int id, @RequestBody ActionDefine action) {
         action.setId(id);
         actionService.update(action);
         return ResponseEntity.ok().body(action);
@@ -52,10 +48,6 @@ public class ActionController {
     public ResponseEntity<PageInfo<ActionDefine>> listAction(ActionDefine action, Integer pageNum,
                                                              Integer pageSize) {
         PageInfo<ActionDefine> page = actionService.findLogActionDefinePage(action, pageNum, pageSize);
-        if (CollectionUtils.isNotEmpty(page.getList())) {
-            return ResponseEntity.ok().body(page);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok().body(page);
     }
 }
